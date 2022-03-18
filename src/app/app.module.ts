c3d8df8af1a6e99b28ac4dc54a5394561ada9342
215656322 <61270620+215656322@users.noreply.github.com>
2022-03-18 14:41:49 +0200

    Admin portal

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { RewardsComponent } from './rewards/rewards.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { AdminComponent } from './admin/admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    RewardsComponent,
+    GalleryComponent,
+    RegistrationFormComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
